5f90592a2daa4976f8bd48d35b9312e6d25ab659
giacomo9999 <gmarchesi888@gmail.com>
2020-12-27 15:24:00 -0500

    Basic fetching and displaying done.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,16 +4,31 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
+  state = { posts: [] };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Jim G.",
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    });
+  }
+
   render() {
+    const posts = this.state.posts.map((post) => (
+      <Post key={post.id} title={post.title} author={post.author} />
+    ));
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
