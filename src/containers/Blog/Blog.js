bef07ffbf8653a391f7bfcb7ceea83127baec240
giacomo9999 <gmarchesi888@gmail.com>
2020-12-30 13:19:51 -0500

    Added links for routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -46,7 +46,19 @@ class Blog extends Component {
     }
 
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-posts">New Posts</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className="Posts">{posts}</section>
         <section>
           <FullPost postId={this.state.selectedPostId} />
